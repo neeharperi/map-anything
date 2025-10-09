44cbc5a4a3960cab50c449797afef200b88b94b7
Pierre Moulon @ Meta <74376766+SeaOtocinclus@users.noreply.github.com>
2025-10-09 07:08:54 -0700

    Misspellings Fixed and Their Occurrence Counts (#64)
    * Misspellings Fixed and Their Occurrence Counts
    * Running Ruff

--- mapanything/models/mapanything/model.py
+++ mapanything/models/mapanything/model.py
@@ -121,7 +121,7 @@ class MapAnything(nn.Module, PyTorchModelHubMixin):
         """
         super().__init__()
 
-        # Initalize the attributes
+        # Initialize the attributes
         self.name = name
         self.encoder_config = encoder_config
         self.info_sharing_config = info_sharing_config
@@ -256,7 +256,7 @@ class MapAnything(nn.Module, PyTorchModelHubMixin):
         else:
             self.custom_positional_encoding = None
 
-        # Add dependecies to info_sharing_config
+        # Add dependencies to info_sharing_config
         info_sharing_config["module_args"]["input_embed_dim"] = (
             self.encoder.enc_embed_dim
         )
@@ -267,7 +267,7 @@ class MapAnything(nn.Module, PyTorchModelHubMixin):
         # Initialize Multi-View Transformer
         if self.info_sharing_return_type == "no_intermediate_features":
             # Returns only normalized last layer features
-            # Intialize multi-view transformer based on type
+            # Initialize multi-view transformer based on type
             if self.info_sharing_type == "cross_attention":
                 self.info_sharing = MultiViewCrossAttentionTransformer(
                     **info_sharing_config["module_args"]
@@ -370,7 +370,7 @@ class MapAnything(nn.Module, PyTorchModelHubMixin):
             # Initialize Dense Prediction Head for all views
             self.dense_head = LinearFeature(**pred_head_config["feature_head"])
         elif "dpt" in self.pred_head_type:
-            # Initialze Dense Predction Head for all views
+            # Initialize Dense Prediction Head for all views
             self.dpt_feature_head = DPTFeature(**pred_head_config["feature_head"])
             self.dpt_regressor_head = DPTRegressionProcessor(
                 **pred_head_config["regressor_head"]
